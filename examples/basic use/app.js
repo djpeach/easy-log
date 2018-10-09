26d6b59e4cfcd0fcb6f45490474df09667163ebe
Daniel Peach <dpeaches96@gmail.com>
2018-10-09 19:25:10 -0400

    fixed paths to module in examples

--- examples/basic use/app.js
+++ examples/basic use/app.js
@@ -1,16 +1,6 @@
 const dbLogger = require('../../')('db', { formatted: true });
 const routeLogger = require('../../')('route');
 
-// const dbLogger = logger('db');
-dbLogger('testing the db logger');
-routeLogger('testing the route logger 1');
-routeLogger.enable();
-routeLogger('testing the route logger 2');
-routeLogger('testing the route logger 2');
-routeLogger.disable();
-routeLogger('testing the route logger 3');
-routeLogger.enable();
-routeLogger('testing the route logger 2');
-routeLogger('testing the route logger 2');
+
 
 
