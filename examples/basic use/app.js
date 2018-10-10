c4d2aaac04eec6bb08a23e685cb6d3ce83a37ce8
Daniel Peach <dpeaches96@gmail.com>
2018-10-09 20:15:39 -0400

    i1 consistent colors resolved (#2)
    Colors now calculated based on namespace value

--- examples/basic use/app.js
+++ examples/basic use/app.js
@@ -1,7 +1,12 @@
 const dbLogger = require('../../')('db', { formatted: true });
 const routeLogger = require('../../')('route');
 
+dbLogger.enable();
+routeLogger.enable();
 
+
+dbLogger('testing the db Logger');
+routeLogger('testing the route Logger');
 dbLogger('This one will not work');
 dbLogger.enable()
 dbLogger('Now it will work');
