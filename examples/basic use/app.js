b9f03fdef0d0af1b94ff929cb6262b275d21f7a9
Daniel Peach <dpeaches96@gmail.com>
2018-10-09 20:07:28 -0400

    Update app.js

--- examples/basic use/app.js
+++ examples/basic use/app.js
@@ -2,5 +2,8 @@ const dbLogger = require('../../')('db', { formatted: true });
 const routeLogger = require('../../')('route');
 
 
+dbLogger('This one will not work');
+dbLogger.enable()
+dbLogger('Now it will work');
 
-
+// The same applies for routeLogger()
