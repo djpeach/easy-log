9da4698296cf955eccb1f71c4ae42b654b3743f2
Daniel Peach <dpeaches96@gmail.com>
2018-10-09 20:04:50 -0400

    Colors now calculated based on namespace value

--- examples/basic use/app.js
+++ examples/basic use/app.js
@@ -1,6 +1,8 @@
 const dbLogger = require('../../')('db', { formatted: true });
 const routeLogger = require('../../')('route');
 
+dbLogger.enable();
+routeLogger.enable();
 
-
-
+dbLogger('testing the db Logger');
+routeLogger('testing the route Logger');
