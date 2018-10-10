13cf641cb3a840bc37994e85a54c266917e38a7f
Daniel Peach <dpeaches96@gmail.com>
2018-10-10 00:11:07 -0400

    added test for function trace

--- examples/basic use/app.js
+++ examples/basic use/app.js
@@ -4,7 +4,6 @@ const routeLogger = require('../../')('route');
 dbLogger.enable();
 routeLogger.enable();
 
-
 dbLogger('testing the db Logger');
 routeLogger('testing the route Logger');
 dbLogger('This one will not work');
@@ -12,3 +11,9 @@ dbLogger.enable()
 dbLogger('Now it will work');
 
 // The same applies for routeLogger()
+
+function myFunc() {
+    dbLogger('This is inside a function');
+}
+
+myFunc();
