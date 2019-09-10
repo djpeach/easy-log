5ffaad5d252351ed2a7cb94ba8b631a0d2efeaa3
Daniel Peach <dpeaches96@gmail.com>
2019-09-10 12:35:56 -0400

    updated example and coloring whole logger now

--- index.js
+++ index.js
@@ -27,9 +27,8 @@ function createLogger(namespace = '', { colorCode, includeFunction, includeFile,
                 , logTraceBar = logger.includeFile || logger.includeFunction || logger.includeLineNumber ? ' |' : ''
                 , logTrace = `${logTraceBar}${functionName}${fileName}${lineNumber}`
                 , useArrow = data === '' ? '' : ' -> '
-                , totalPrefix = namespace.padStart
-                    (longestNamespaceLength) + logTrace + useArrow;
-            console.log(color(totalPrefix) + `${data}`);
+                , totalPrefix = namespace.padStart(longestNamespaceLength, ' ') + logTrace + useArrow;
+            console.log(color(totalPrefix + `${data}`));
         }
     }
 
